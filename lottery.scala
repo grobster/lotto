bf95b35b835d38fe18e8206bf4ae19d204a4a316
grobster <jquarles@grobster.com>
2016-01-06 10:20:05 -0500

    generalized the listMatch function

--- lottery.scala
+++ lottery.scala
@@ -6,9 +6,9 @@ package com.grobster.game {
 	case class Player(lastName: String, firstName: String, amountPaid: Int)
 
 	object NumberMatch {
-		def countNumberNumsMatching(tries: List[Int], master: List[Int]): Int = {
+		def listMatch[A](tries: List[A], master: List[A]): Int = {
 			@scala.annotation.tailrec
-			def _count(tries: List[Int], master: List[Int], count: Int): Int = tries match {
+			def _count(tries: List[A], master: List[A], count: Int): Int = tries match {
 				case Nil => count
 				case head :: tail if(master.contains(head)) => _count(tail, master, 1 + count)
 				case head :: tail if(!master.contains(head)) => _count(tail, master, count)
@@ -39,7 +39,7 @@ import com.grobster.util._
 			val totalReceived = players.map(x => x.amountPaid).sum
 			val lottery = LotteryDrawing("Mega-Winnings", java.time.LocalDate.now)
 			val lotteryNumbers = List(List(12, 44, 32, 53, 50), List(45,17,55,23,42,53), List(34,5, 46,53))
-			val winnings = lotteryNumbers.map(x => NumberMatch.countNumberNumsMatching(x, lottery.getWinningNumbers))
+			val winnings = lotteryNumbers.map(x => NumberMatch.listMatch(x, lottery.getWinningNumbers))
 			println("Amount Received: " + totalReceived)
 			println("number of matched winning numbers: " + winnings)
 		}
